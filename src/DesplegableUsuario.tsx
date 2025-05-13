dab286e59e9eb1315f3e58105eeb91db93c616e6
Matias Castillo <maticomadran@gmail.com>
2025-05-12 22:14:55 -0300

    Pagina inicio terminada

--- src/DesplegableUsuario.tsx
+++ src/DesplegableUsuario.tsx
@@ -15,20 +15,20 @@ export default function DesplegableUsuario({alCerrarSesion}: Props) {
                 <path d="M39 37.375C43.4873 37.375 47.125 33.7373 47.125 29.25C47.125 24.7627 43.4873 21.125 39 21.125C34.5127 21.125 30.875 24.7627 30.875 29.25C30.875 33.7373 34.5127 37.375 39 37.375Z" stroke="#FAF8F5" stroke-width="4.875" stroke-linecap="round" stroke-linejoin="round"/>
                 <path d="M29.2501 55.25L58.5001 19.5M19.5001 39L45.5001 8.12497M22.7501 16.25L32.5001 24.375M39.0326 71.5C37.8821 71.5 37.3459 71.0092 36.7544 69.3582C35.1944 63.8657 32.2759 59.1207 28.7366 54.665C25.5321 50.3847 20.8066 45.513 18.3496 39.962C11.5246 24.8787 22.1034 6.47397 38.9774 6.49997C56.3161 6.45122 66.7421 25.5157 59.3321 40.7712C57.1551 44.6197 54.6092 48.2475 51.7304 51.6035C47.2421 56.9042 43.2089 62.6437 41.2946 69.368C40.8526 70.6647 40.2871 71.5 39.0326 71.5Z" stroke="#FAF8F5" stroke-width="4.875" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                <label>Mis Direcciones</label>
+                <label className="hover:cursor-pointer">Mis Direcciones</label>
             </Link>
             <Link className="flex items-center border-b h-12" to={"misOrdenes"}>
                 <svg className="ml-5" width="40" height="30" viewBox="0 0 52 63" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M42.25 4H9.75C6.16015 4 3.25 6.91015 3.25 10.5V52.75C3.25 56.3399 6.16015 59.25 9.75 59.25H42.25C45.8399 59.25 48.75 56.3399 48.75 52.75V10.5C48.75 6.91015 45.8399 4 42.25 4Z" stroke="#FAF8F5" stroke-width="6.5"/>
                 <path d="M16.25 20.25H35.75M16.25 33.25H35.75M16.25 46.25H29.25" stroke="#FAF8F5" stroke-width="6.5" stroke-linecap="round"/>
                 </svg>
-                <label>Mis Ordenes</label>
+                <label className="hover:cursor-pointer">Mis Ordenes</label>
             </Link>
             <Link className="flex items-center border-b h-12" to={"miPerfil"}>
             <svg className="ml-5" width="40" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M39 13C35.9832 13 33.0899 14.1984 30.9567 16.3317C28.8234 18.4649 27.625 21.3582 27.625 24.375C27.625 27.3918 28.8234 30.2851 30.9567 32.4183C33.0899 34.5516 35.9832 35.75 39 35.75C42.0168 35.75 44.9101 34.5516 47.0433 32.4183C49.1766 30.2851 50.375 27.3918 50.375 24.375C50.375 21.3582 49.1766 18.4649 47.0433 16.3317C44.9101 14.1984 42.0168 13 39 13ZM21.125 24.375C21.125 19.6343 23.0083 15.0877 26.3605 11.7355C29.7127 8.38325 34.2593 6.5 39 6.5C43.7407 6.5 48.2873 8.38325 51.6395 11.7355C54.9918 15.0877 56.875 19.6343 56.875 24.375C56.875 29.1157 54.9918 33.6623 51.6395 37.0145C48.2873 40.3667 43.7407 42.25 39 42.25C34.2593 42.25 29.7127 40.3667 26.3605 37.0145C23.0083 33.6623 21.125 29.1157 21.125 24.375ZM9.75 61.75C9.75 57.4402 11.462 53.307 14.5095 50.2595C17.557 47.212 21.6902 45.5 26 45.5H52C56.3098 45.5 60.443 47.212 63.4905 50.2595C66.538 53.307 68.25 57.4402 68.25 61.75V71.5H9.75V61.75ZM26 52C23.4141 52 20.9342 53.0272 19.1057 54.8557C17.2772 56.6842 16.25 59.1641 16.25 61.75V65H61.75V61.75C61.75 59.1641 60.7228 56.6842 58.8943 54.8557C57.0658 53.0272 54.5859 52 52 52H26Z" fill="#FAF8F5"/>
             </svg>
-            <label>Mi Perfil</label>
+            <label className="hover:cursor-pointer">Mi Perfil</label>
             </Link>
             <button className="flex items-center h-12" onClick={alCerrarSesion} type="button">
                 <svg className="ml-5" width="40" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
---
+++ src/Inicio.tsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import SliderImagenes from "./SliderImagenes";
+import { Link } from "react-router-dom";
+
+interface Ejemplo{
+    nombre: string
+    imagen: string
+}
+
+export default function Inicio() {
+    
+    const [promos, setPromos] = useState<any[]>([])
+
+    //Cambiar por fetch de promociones del backend
+    useEffect(()=>{
+        let objetos: Ejemplo[] = [
+            {nombre: "uno", imagen: "pizza.jpg"},
+            {nombre: "2", imagen: "logoCarrito.jpg"},
+            {nombre: "3", imagen: "miniUsuario.jpg"}
+
+        ]
+
+        setPromos(objetos)
+
+    }, [])
+
+    //Estilos
+    //General
+    const titulos = "text-6xl"
+    //Como funciona
+    const secciones = "bg-[#F2B28C80] mx-auto h-auto w-2/3 rounded-4xl text-center my-15 p-[5%]"
+    const imgComoFunciona = "rounded-[20rem] w-75 h-75 m-auto object-cover"
+    const subtitulos = "m-3 text-4xl"
+    const parrafo = "m-3 text-2xl"
+
+    return(
+
+        <>
+        
+        <div className="bg-[#333333] h-full w-1/1 text-white font-[Playfair_Display]">
+
+            {/* Carrusel de promociones */}
+            <div className="grid grid-cols-[2fr_2fr] p-[67px] gap-5">
+
+                <div className="flex flex-col text-center">
+
+                    <h1 className="text-5xl ">Ordena tus comidas desde la comodidad de tu casa</h1>
+                    <Link className="bg-[#D93F21] rounded-lg m-auto text-4xl flex items-center p-5" to="/catalogo">
+                        <h2 className="text-center">Catalogo</h2>
+                    </Link>
+
+                </div>
+                <div className="flex items-center w-3/4 m-auto h-[300px]">
+                    <SliderImagenes data={promos}/>
+                </div>
+                
+
+            </div>
+
+            {/* Como funciona*/}
+            <div className={secciones}>
+
+                <div>
+                    <h1 className={titulos}>¿Como funciona?</h1>
+                </div>
+                <div className="grid grid-cols-3 mt-10 gap-10">
+                    <div>
+                        <img className={imgComoFunciona} src="/img/elegiTuComida.jpg"/>
+                        <h2 className={subtitulos}>1. Elegí tu comida</h2>
+                        <p className={parrafo}>Busca en nuestro catalogo y ordena por la app</p>
+                    </div>
+                    <div>
+                        <img className={imgComoFunciona} src="/img/haceTuOrden.jpg"/>
+                        <h2 className={subtitulos}>2. Hacé tu orden</h2>
+                        <p className={parrafo}>¡Facil y rapido! Podes pagar online o en la entrega</p>
+                    </div>
+                    <div>
+                        <img className={imgComoFunciona} src="/img/recibiTuPedido.jpg"/>
+                        <h2 className={subtitulos}>3. Recibí el pedido</h2>
+                        <p className={parrafo}>Recibi tu pedido en tu domicilio o pasa a retirarlo al local</p>
+                    </div>
+                </div>
+            </div>
+
+            {/*Sobre nosotros */}
+            <div className={secciones}>
+                
+                <div className="grid grid-cols-[3fr_2fr] gap-5">
+
+                    <div className="grid grid-rows-[1fr_3fr] text-left gap-5">
+                        <div>
+                            <h1 className={titulos}>Sobre Nosotros</h1>
+                        </div>
+                        
+                        <div className="flex">
+                            <p className="text-4xl">
+                                Bienvenido a El Buen sabor, un lugar 
+                                donde la pasión por la buena comida y el 
+                                buen servicio nos define. 
+                                Desde nuestros inicios, nos hemos dedicado 
+                                a ofrecer productos de calidad y con el mejor sabor.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    
+                    <img className="m-auto h-[35rem] rounded-4xl object-cover" src="/img/imgSobreNosotros.jpg" alt="" />
+                    
+
+                </div>
+
+            </div>
+
+            
+            {/**Contactanos */}
+            <div className={secciones}>
+                <div className="text-left grid grid-rows-[1fr_5fr] gap-10">
+                    <div>
+                        <h1 className={titulos}>Contactanos</h1>
+                    </div>
+                    <div className="grid grid-rows-4 gap-8">
+                        <div className="flex items-center">
+                            <img src="/svg/instagram.svg" alt="" />
+                            <h2 className={subtitulos}>@ElBuenSaborOk</h2>
+                        </div>
+                        <div className="flex items-center">
+                            <img src="/svg/facebook.svg" alt="" />
+                            <h2 className={subtitulos}>@ElBuenSaborOk</h2>
+                        </div>
+                        <div className="flex items-center">
+                            <img src="/svg/whatsapp.svg" alt="" />
+                            <a href="callto:+5492615874398" className={subtitulos}>+54 9 2615874398</a>
+                        </div>
+                        <div className="flex items-center">
+                            <img src="/svg/gmail.svg" alt="" />
+                            <a href="mailto:ElBuenSabor@gmail.com" className={subtitulos}>ElBuenSabor@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        </>
+        
+    )
+
+}
